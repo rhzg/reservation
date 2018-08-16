031622e1a37f1985dfa9ca91d77232e34ee95c1f
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2018-08-16 16:44:55 +0200

    remove static room inits

--- src/main/java/de/fraunhofer/iosb/Initialization.java
+++ src/main/java/de/fraunhofer/iosb/Initialization.java
@@ -55,18 +55,6 @@ public class Initialization implements CommandLineRunner {
     @Override
     @Transactional
     public void run(String... args) throws Exception {
-        Room r18 = new Room("hiwi_1", "HIWI 1", "token");
-        r18.getBleIds().add("10655:42221");
-        r18.getBleIds().add("62834:31868");
-        repoRoom.save(r18);
-
-        Room r19 = new Room("hiwi_2", "HIWI 2", "token");
-        r19.getBleIds().add("623:61687");
-        repoRoom.save(r19);
-
-        Room r22 = new Room("C8-22", "http://localhost:8080/rooms/2", "neki token");
-        repoRoom.save(r22);
-
         Room all = new Room("all", "http://localhost:8080/rooms/all", "neki token");
         repoRoom.save(all);
 
@@ -75,31 +63,14 @@ public class Initialization implements CommandLineRunner {
         userRepo.save(userAdmin);
 
         User userAdmin1 = new User("admin1@fer.hr", "admin1", "Viseslav Admin", "admin@fer.hr", "0");
-        userAdmin1.getFavorites().put(r22.getRoomID(), r22);
         userAdmin1.setToken("e67a3e52-24d8-44cc-bec7-5bd2371c55d9");
         userRepo.save(userAdmin1);
-
-        User user18 = new User("user18", "user18", "User18 User18", "user18@fer.hr", "1");
-        user18.setToken("1fa5fbc4-83b7-49cf-b40b-5cc61a3cf657");
-        user18.setCurentRoom(r18);
-        userRepo.save(user18);
-
-        User user22 = new User("user22", "user22", "User22 User22", "user22@fer.hr", "2");
-        user22.setNfccode("12345678");
-        user22.setCurentRoom(r18);
-        userRepo.save(user22);
 
         Role roleAdmin = new Role();
         roleAdmin.setRoom(all);
         roleAdmin.setRole("admin");
         roleAdmin.setUser(userAdmin1);
         roleRepo.save(roleAdmin);
-
-        addUserForRoom(r18, "bb46865", "bilokoji", "Boris Belaj", "boris.belaj@fer.hr", "3");
-        addUserForRoom(r18, "luka", "svejedno", "Luka Ušalj", "luka.usalj@fer.hr", "4");
-        addUserForRoom(r18, "vatroslav", "neznam", "Vatroslav Bajt", "vatroslav.bajt@fer.hr", "5");
-        addUserForRoom(r18, "mario ", "kusek", "Mario Kušek", "mario.kusek@fer.hr", "6");
-        addUserForRoom(r18, "marko", "pavelic", "Marko Pavelić", "marko.pavelic@fer.hr", "7");
 
         /*
          * use the following code only during testing service =
@@ -114,8 +85,6 @@ public class Initialization implements CommandLineRunner {
             Room newRoom = new Room(sbr.getName(), sbr.getDescription(), sbr.getToken());
             for (SbBeacon b: sbr.getAssignedBeacons()) {
               String beaconName = b.getUUID() + ":" + b.getMajor() + ":" + b.getMinor();
-//              String beaconName = b.getMajor() + ":" + b.getMinor();
-//                String beaconName = b.getUUID();
                 newRoom.getBleIds().add(beaconName);
             }
             repoRoom.save(newRoom);
